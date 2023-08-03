3563a29435adecb6ef7749fa4661924f9756e1f3
Sveta <svetikred77@gmail.com>
2023-08-03 16:29:09 +0300

    add height

--- src/components/Data/Data.styled.js
+++ src/components/Data/Data.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: 80vh;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
