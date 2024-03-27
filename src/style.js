89b0a4fc6a73907b11cdcde57f89ff085716db9a
Sveta <svetikred77@gmail.com>
2024-03-27 12:03:42 +0200

    fix img

--- src/style.js
+++ src/style.js
@@ -5,7 +5,6 @@ import img from '../src/assets/bgImage.jpg';
 export const Global = createGlobalStyle`
 
 body {
-background-color: #fff;
 background-image: url('${img}');
   background-position: center;
   background-repeat: no-repeat;
